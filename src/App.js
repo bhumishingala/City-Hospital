9e5c1e56054be6bdeed0965f837c3d7befd9b0c2
bhumishingala <shingalabhumi28@gmail.com>
2022-06-09 16:02:01 +0530

    city-hospital

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './compoents/header/Header';
+import Footer from './compoents/footer/Footer';
+import Home from './containers/home/Home';
 
 function App() {
   return (
     <div>
-  
+        <Header />
+        <Home />
+        <Footer />
     </div>
   );
 }
