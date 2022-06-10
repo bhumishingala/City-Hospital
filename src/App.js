ee13d494f6fca3a53176f0505a8aab30dcaa843a
bhumishingala <shingalabhumi28@gmail.com>
2022-06-10 16:20:00 +0530

    integrat

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import Header from './compoents/header/Header';
 import Footer from './compoents/footer/Footer';
 import Home from './containers/home/Home';
+import { Route, Switch } from 'react-router-dom';
+import Department from './containers/department/Department';
+import Doctor from './containers/doctors/Doctor';
+import Aboutus from './containers/about us/Aboutus';
+import Contact from './containers/contact/Contact';
 
 function App() {
   return (
     <div>
         <Header />
-        <Home />
+
+        <Switch>
+            <Route path={"/"} exact component={Home}></Route>
+            <Route path={"/department"} exact component={Department}></Route>
+            <Route path={"/doctors"} exact component={Doctor}></Route>
+            <Route path={"/aboutus"} exact component={Aboutus}></Route>
+            <Route path={"/contact"} exact component={Contact}></Route>
+        </Switch>
+        
         <Footer />
     </div>
   );
