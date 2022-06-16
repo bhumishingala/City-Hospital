cf777db05fab7ec217e11840f4a19f937b9289c6
bhumishingala <shingalabhumi28@gmail.com>
2022-06-16 10:26:32 +0530

    listing

--- src/compoents/list/List.js
+++ src/compoents/list/List.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { Card ,CardBody,CardTitle,CardSubtitle,CardText} from 'reactstrap';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 
 function List({ data }) {
-    {
-        data.map((o, i) => {
-            return (
-                <div key={i}>
-                    <Card>
-                        <CardBody>
-                            <CardTitle tag="h5">
-                                {o.name}
-                            </CardTitle>
-                            <CardSubtitle
-                                className="mb-2 text-muted"
-                                tag="h6"
-                            >
-                                {o.price}
-                            </CardSubtitle>
-                            <CardText>
-                               {o.expiry}
-                            </CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            )
-        })
-    }
+    return (
+        <div>
+            {
+                data.map((o, i) => {
+                    return (
+                        <div key={i}>
+                            <Card>
+                                <CardBody>
+                                    <CardTitle tag="h5">
+                                        {o.name}
+                                    </CardTitle>
+                                    <CardSubtitle
+                                        className="mb-2 text-muted"
+                                        tag="h6"
+                                    >
+                                        {o.price}
+                                    </CardSubtitle>
+                                    <CardText>
+                                        {o.expiry}
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default List;
