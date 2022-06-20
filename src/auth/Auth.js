49e05521e8e0455e9fbb7500345a53c4a6167894
bhumishingala <shingalabhumi28@gmail.com>
2022-06-20 11:05:40 +0530

    yup and fromik

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -25,73 +25,71 @@ function Auth(props) {
     const { handleChange, errors, handleSubmit } = formik;
 
     return (
-        <div class="text-center">
-            <section id="appointment" className="appointment">
-                <div className="container">
-                    <div className="section-title">
-                        {
-                            reset === "true" ?
-                                <h2>Forgot Password</h2>
+        <section id="appointment" className="appointment">
+            <div className="container">
+                <div className="section-title">
+                    {
+                        reset === "true" ?
+                            <h2>Forgot Password</h2>
+                            :
+                            usertype === "Login" ?
+                                <h2>Login</h2>
                                 :
-                                usertype === "Login" ?
-                                    <h2>Login</h2>
-                                    :
-                                    <h2>Sign Up</h2>
-                        }
-                    </div>
-                    <Formik values={formik}>
-                        <Form onSubmit={handleSubmit} className="php-email-form">
-                            <div className="row">
-                                {
-                                    reset === "true" ?
-                                        null
-                                        :
-                                        usertype === "Login" ?
-                                            null
-                                            :
-                                            <div className="col-md-4 form-group">
-                                                <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                            </div>
-                                }
-                            </div>
-                            <div className='row'>
-                                <div className="col-md-4 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" onChange={handleChange} />
-                                    <p>{errors.email}</p>
-                                </div>
-                            </div>
+                                <h2>Sign Up</h2>
+                    }
+                </div>
+                <Formik values={formik}>
+                    <Form onSubmit={handleSubmit} className="php-email-form">
+                        <div className="row">
                             {
                                 reset === "true" ?
                                     null
                                     :
-                                    <div className='row'>
-                                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                                            <input type="password" className="form-control" name="password" id="password" placeholder="Your password" data-rule="password" data-msg="Please enter a password" onChange={handleChange} />
-                                            <p>{errors.password}</p>
-                                        </div>
-                                    </div>
-                            }
-                            {
-                                reset === "true" ?
-                                    <div class="text-center"><button type="submit">Submit</button></div>
-                                    :
                                     usertype === "Login" ?
-                                        <div class="text-center"><button type="submit">Login</button></div>
+                                        null
                                         :
-                                        <div class="text-center"><button type="submit">Signup</button></div>
+                                        <div className="col-md-4 form-group">
+                                            <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                        </div>
                             }
-                            {
+                        </div>
+                        <div className='row'>
+                            <div className="col-md-4 form-group mt-3 mt-md-0">
+                                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" onChange={handleChange} />
+                                <p>{errors.email}</p>
+                            </div>
+                        </div>
+                        {
+                            reset === "true" ?
+                                null
+                                :
+                                <div className='row'>
+                                    <div className="col-md-4 form-group mt-3 mt-md-0">
+                                        <input type="password" className="form-control" name="password" id="password" placeholder="Your password" data-rule="password" data-msg="Please enter a password" onChange={handleChange} />
+                                        <p>{errors.password}</p>
+                                    </div>
+                                </div>
+                        }
+                        {
+                            reset === "true" ?
+                                <div class="text-center"><button type="submit">Submit</button></div>
+                                :
                                 usertype === "Login" ?
-                                    <p>create an account ?<button onClick={() => { setReset("false"); setUsertype("Signup") }}>Signup</button></p>
+                                    <div class="text-center"><button type="submit">Login</button></div>
                                     :
-                                    <p>allready account ?<button onClick={() => { setReset("false"); setUsertype("Login") }}>Login</button></p>
-                            }
-                            <button onClick={() => setReset("true")}>Forgot Your Password ?</button>
-                        </Form>
-                    </Formik>
-                </div>
-            </section>
-        </div>
+                                    <div class="text-center"><button type="submit">Signup</button></div>
+                        }
+                        {
+                            usertype === "Login" ?
+                                <p>create an account ?<button onClick={() => { setReset("false"); setUsertype("Signup") }}>Signup</button></p>
+                                :
+                                <p>allready account ?<button onClick={() => { setReset("false"); setUsertype("Login") }}>Login</button></p>
+                        }
+                        <button onClick={() => setReset("true")}>Forgot Your Password ?</button>
+                    </Form>
+                </Formik>
+            </div>
+        </section>
     );
 }
 
