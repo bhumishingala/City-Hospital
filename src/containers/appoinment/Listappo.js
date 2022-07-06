9f74a20789d8dbe0d031580cab018aa7d73caece
bhumishingala <bhumishingala>
2022-07-06 15:48:47 +0530

    redirect list appo.

--- src/containers/appoinment/Listappo.js
+++ src/containers/appoinment/Listappo.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import { Formik } from 'formik';
+import React, { useEffect, useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
 
 function Listappo(props) {
+    const history = useHistory();
+    let [data, setData] = useState([]);
+
+    const gData = () => {
+        let localData = JSON.parse(localStorage.getItem("book-apt"));
+
+        setData(localData);
+    }
+
+    useEffect(() => {
+        gData();
+    }, []);
+
+    const handleDelete = (id) => {
+        let localData = JSON.parse(localStorage.getItem("book-apt"));
+
+        let fData = localData.filter((f) => f.id !== id);
+
+        localStorage.setItem("book-apt",JSON.stringify(fData));
+
+        console.log(fData,localData);
+        gData();
+    }
+
+    const  redirectB_app = (id) => {
+        history.push("/Book_apt", {id :id});
+        console.log(this.props.location.state.id);
+        console.log(id);
+    }
+
     return (
         <div>
-            <h1>List An Appointment</h1>
+            <main>
+                <section id="appointment" className="appointment">
+                    <div className="container">
+                        <div className="section-title">
+                            <h2>List an Appointment</h2>
+                        </div>
+                        <div className='row'>
+                            <div className='col-6'>
+                                <NavLink exact to={"/Book_apt"} >Book An Appointment</NavLink>
+                            </div>
+                            <div className='col-6'>
+                                <NavLink exact to={"/list_apt"} >List An Appointment</NavLink>
+                            </div>
+                        </div>
+                        {
+                            data.map((d , i) => {
+                                return (
+                                    <Card key={i}>
+                                        <CardBody>
+                                            <CardTitle tag="h5">
+                                                {d.name}
+                                            </CardTitle>
+                                            <CardSubtitle
+                                                className="mb-2 text-muted"
+                                                tag="h6"
+                                            >
+                                                {d.email}
+                                            </CardSubtitle>
+                                            <CardText>
+                                              {d.phone}
+                                            </CardText>
+                                            <Button onClick={() => redirectB_app(d.id)}>Edit</Button>
+                                            <Button onClick={() => handleDelete(d.id)}>Delete</Button>
+                                        </CardBody>
+                                    </Card>
+                                )
+                            })
+                        }
+                    </div>
+                </section>
+            </main>
         </div>
     );
 }
